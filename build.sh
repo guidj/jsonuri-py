6d0e401c9d7d95d8f5de2e7220b0ad9257f43d75
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2016-05-28 22:03:03 +0200

    [#4] updated build.sh script

--- build.sh
+++ build.sh
@@ -1,2 +1,39 @@
 #!/usr/bin/env bash
-python setup.py bdist_wheel --universal
+
+function buildHtmlDoc(){
+
+    SOURCE="${BASH_SOURCE[0]}"
+
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+      DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+      SOURCE="$(readlink "$SOURCE")"
+      [[ ${SOURCE} != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+    cd ${DIR}/docs
+    make html
+    cd ${DIR}
+}
+
+function buildPyPackage(){
+
+    SOURCE="${BASH_SOURCE[0]}"
+
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+      DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+      SOURCE="$(readlink "$SOURCE")"
+      [[ ${SOURCE} != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+    echo ${DIR}
+    cd ${DIR}
+
+    python setup.py bdist_wheel --universal
+    cd ${DIR}
+}
+
+
+buildHtmlDoc
+buildPyPackage
